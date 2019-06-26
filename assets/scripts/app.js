8f8ba6b74b6621e1bdbafea798d81d621903f193
JHankins09 <Hankins.JamesB@gmail.com>
2019-06-26 13:08:13 -0400

    Enable Authentication api calls
    Got authentication API calls all running properly

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,13 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const userEvents = require('./user/events')
+
 $(() => {
   // your JS code goes here
+  $('#sign-up').on('submit', userEvents.onSignUp)
+  $('#sign-in').on('submit', userEvents.onSignIn)
+  $('#change-pw').on('submit', userEvents.onChangePassword)
+  $('#sign-out').on('submit', userEvents.onSignOut)
 })
+// $('#index-books').on('click', bookEvents.onGetBooks)
